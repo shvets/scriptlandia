7b1f20ec236a463bd4629268e63d751466de3367
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2007-02-28 21:52:34 +0000

    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@8 9700c210-9314-0410-bd77-8714033700fb

--- scriptlandia/clean.bat
+++ scriptlandia/clean.bat
@@ -9,6 +9,7 @@ if exist projects\antrun\target rmdir /S /Q projects\antrun\target
 if exist projects\scriptlandia-launcher\target rmdir /S /Q projects\scriptlandia-launcher\target
 if exist projects\scriptlandia-nailgun\target rmdir /S /Q projects\scriptlandia-nailgun\target
 if exist projects\scriptlandia-installer\target rmdir /S /Q projects\scriptlandia-installer\target
+if exist projects\scriptlandia-config\target rmdir /S /Q projects\scriptlandia-config\target
 if exist projects\scriptlandia\target rmdir /S /Q projects\scriptlandia\target
 
 if exist target rmdir /S /Q target
