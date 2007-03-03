176e250e0e2c7c95bfd6cc8ae59a16ce8adf0002
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2007-03-03 19:58:55 +0000

    Small modifications
    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@20 9700c210-9314-0410-bd77-8714033700fb

--- scriptlandia/clean.bat
+++ scriptlandia/clean.bat
@@ -10,6 +10,7 @@ if exist projects\scriptlandia-launcher\target rmdir /S /Q projects\scriptlandia
 if exist projects\scriptlandia-nailgun\target rmdir /S /Q projects\scriptlandia-nailgun\target
 if exist projects\scriptlandia-installer\target rmdir /S /Q projects\scriptlandia-installer\target
 if exist projects\scriptlandia-config\target rmdir /S /Q projects\scriptlandia-config\target
+if exist projects\scriptlandia-startup\target rmdir /S /Q projects\scriptlandia-startup\target
 if exist projects\scriptlandia\target rmdir /S /Q projects\scriptlandia\target
 
 if exist target rmdir /S /Q target
