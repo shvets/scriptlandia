104eab5d597389f4acfb2405bd6e1e33168417b1
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2007-03-18 17:08:37 +0000

    adding classworlds-launcher project
    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@66 9700c210-9314-0410-bd77-8714033700fb

---
+++ scriptlandia/projects/classworlds-launcher/src/main/java/org/sf/scriptlandia/classworlds/launcher/Launcher.java
@@ -0,0 +1,9 @@
+package org.sf.scriptlandia.classworlds.launcher;
+
+public class Launcher extends org.codehaus.classworlds.Launcher {
+
+  public static void main(String[] args) {
+    org.codehaus.classworlds.Launcher.main(args);
+  }
+
+}
