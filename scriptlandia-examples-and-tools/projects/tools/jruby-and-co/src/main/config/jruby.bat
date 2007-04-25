c6cf0cc277add5a0d7eab70437098889ca3a9a8c
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2007-04-25 17:14:52 +0000

    adding support for jruby 0.9.9
    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@161 9700c210-9314-0410-bd77-8714033700fb

--- scriptlandia-examples-and-tools/projects/tools/jruby-and-co/src/main/config/jruby.bat
+++ scriptlandia-examples-and-tools/projects/tools/jruby-and-co/src/main/config/jruby.bat
@@ -3,6 +3,7 @@
 SET JAVA_HOME=@java.home@
 SET SCRIPTLANDIA_VERSION=@scriptlandia.version@
 SET REPOSITORY_HOME=@repository.home@
+SET JRUBY_VERSION=0.9.9
 
 SET JAVA_BINARY="%JAVA_HOME%\bin\java" -server
 
@@ -10,7 +11,8 @@ SET DEPS_FILE=@repository.home@\org\sf\scriptlandia\jruby\%SCRIPTLANDIA_VERSION%
 
 if NOT "@proxy.server.host@" == "" set HTTP_PROXY=http://@proxy.server.host@:@proxy.server.port@
 
-SET SYSTEM_PARAMETERS=-Djruby.home=@repository.home@\jruby\jruby\0.9.8 -Djruby.lib=@repository.home@\jruby\jruby-lib\0.9.8
+SET SYSTEM_PARAMETERS=-Djruby.home=@repository.home@\jruby\jruby\%JRUBY_VERSION% -Djruby.lib=@repository.home@\jruby\jruby-lib\%JRUBY_VERSION%
 
-@scriptlandia.home@\launcher-core.bat "-deps.file.name=%DEPS_FILE%" -main.class.name=org.jruby.Main  %*
+SET MAIN_CLASS_NAME=org.jruby.Main
 
+@scriptlandia.home@\launcher.bat -Xmx256m -Xss1024k -da "-deps.file.name=%DEPS_FILE%" -main.class.name=%MAIN_CLASS_NAME% %*
