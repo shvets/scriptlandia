346ea418a7df04710541173e42302dac51402664
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2007-05-01 18:29:19 +0000

    bug fix
    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@170 9700c210-9314-0410-bd77-8714033700fb

--- scriptlandia/projects/scriptlandia-config/bin/launcher/processArgs.bat
+++ scriptlandia/projects/scriptlandia-config/bin/launcher/processArgs.bat
@@ -22,6 +22,10 @@ set PARAM=%TEMP:~0,4%
 
 if "%PARAM%"=="-ng" goto prepareNailgunMode
 
+set PARAM=%TEMP:~0,19%
+
+if "%PARAM%"=="-Djava.library.path" goto prepareJavaLibraryPath
+
 set CMD_LINE_ARGS=%CMD_LINE_ARGS% "%~1%"
 
 :endSetupArgs
@@ -49,6 +53,10 @@ goto endSetupArgs
 
 :prepareNailgunMode
 set NAILGUN_MODE=true
+goto endSetupArgs
+
+:prepareJavaLibraryPath
+set PARAMETERS=%PARAMETERS% "%~1%"
 goto endSetupArgs
 
 :doneStart
