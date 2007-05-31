4b4ee008ccee51d0d069b5696f8960c6ea6b125c
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2007-05-31 20:46:34 +0000

    adding swt examples
    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@280 9700c210-9314-0410-bd77-8714033700fb

---
+++ scriptlandia-examples-and-tools/projects/tech-examples/swt/groovy/test1.groovy
@@ -0,0 +1,27 @@
+// test1.groovy 
+
+print "hello!!!"
+
+import org.eclipse.swt.SWT
+import org.eclipse.swt.widgets.*
+import org.eclipse.swt.layout.RowLayout as Layout
+
+def display = new Display()
+def shell = new Shell(display)
+
+shell.layout = new Layout(SWT.VERTICAL)
+
+shell.text = 'Groovy / SWT Test'
+
+def label = new Label(shell, SWT.NONE)
+label.text = 'Simple demo of Groovy and SWT'
+shell.defaultButton = new Button(shell, SWT.PUSH)
+shell.defaultButton.text = '  Push Me  '
+
+shell.pack()
+shell.open()
+
+while (!shell.disposed) {
+    if (!shell.display.readAndDispatch()) shell.display.sleep()
+}
+
