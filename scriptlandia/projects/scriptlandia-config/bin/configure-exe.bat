86c126d7392f57c177788b4d1c75fdb64b15c1c2
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2007-06-14 19:07:17 +0000

    simplifying scripts
    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@304 9700c210-9314-0410-bd77-8714033700fb

--- scriptlandia/projects/scriptlandia-config/bin/configure-exe.bat
+++ scriptlandia/projects/scriptlandia-config/bin/configure-exe.bat
@@ -1,5 +1,3 @@
 SET SCRIPTLANDIA_HOME=@scriptlandia.home@
 
-rem set SYSTEM_PARAMETERS=-Dbeanshell.version=@beanshell.version@ -Dant.version.internal=@ant.version.internal@
-
 start %SCRIPTLANDIA_HOME%\launcher-gui.exe -config
--- scriptlandia/projects/scriptlandia-config/bin/configure.bat
+++ scriptlandia/projects/scriptlandia-config/bin/configure.bat
@@ -1,7 +1,5 @@
 @echo off
 
-rem set SYSTEM_PARAMETERS=-Dbeanshell.version=@beanshell.version@ -Dant.version.internal=@ant.version.internal@
-
 SET SCRIPTLANDIA_HOME=@scriptlandia.home@
 
 %SCRIPTLANDIA_HOME%\launcher.bat -config %*
