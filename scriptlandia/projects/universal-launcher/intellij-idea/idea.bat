673aa6f722dd7f3431234a1b022da87a1a71e548
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2007-06-21 22:20:26 +0000

    work in progress for universal launcher
    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@328 9700c210-9314-0410-bd77-8714033700fb

--- scriptlandia/projects/universal-launcher/intellij-idea/idea.bat
+++ scriptlandia/projects/universal-launcher/intellij-idea/idea.bat
@@ -2,4 +2,6 @@ rem SET IDEA_HOME="C:\Editors\IntelliJ IDEA 7020"
 rem SET IDEA_PROPERTIES=%IDEA_HOME%\bin\idea.properties
 rem SET PATH="%IDEA_HOME%\bin";%PATH%
 
-..\launcher.bat "-deps.file.name=%CD%\idea-deps.xml" "-main.class.name=com.intellij.idea.Main" -gui %*
+rem ..\launcher.bat "-deps.file.name=%CD%\idea-deps.xml" "-main.class.name=com.intellij.idea.Main" -gui %*
+
+..\launcher.bat "-deps.file.name=%CD%\idea-deps.classpath" "-main.class.name=com.intellij.idea.Main" -gui %*
