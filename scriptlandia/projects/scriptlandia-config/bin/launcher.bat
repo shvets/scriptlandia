5f3e88dfd608c4e792466389417a590d71ffc2d8
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2007-06-23 15:15:09 +0000

    integration with universal launcher"
    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@331 9700c210-9314-0410-bd77-8714033700fb

--- scriptlandia/projects/scriptlandia-config/bin/launcher.bat
+++ scriptlandia/projects/scriptlandia-config/bin/launcher.bat
@@ -3,6 +3,7 @@
 set JAVA_HOME=@java.home@
 set MOBILE_JAVA_HOME=@mobile.java.home@
 set SCRIPTLANDIA_HOME=@scriptlandia.home@
+set LAUNCHER_HOME=@launcher.home@
 
 SET NAILGUN=@repository.home@\com\martiansoftware\nailgun-bin\@nailgun.version@\ng
 
@@ -10,4 +11,4 @@ set APP=%~nx0
 set APP_NAME=%APP:~0,-4%
 set APP=%SCRIPTLANDIA_HOME%\%APP:~0,-4%
 
-call %SCRIPTLANDIA_HOME%\launcher\app.bat %*
+call %LAUNCHER_HOME%\launcher-core.bat %*
