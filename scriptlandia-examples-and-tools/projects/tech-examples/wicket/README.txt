f2969b85d3b51b99f42c35502db0f8846d3f59c6
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2007-07-24 19:37:40 +0000

    updating example
    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@429 9700c210-9314-0410-bd77-8714033700fb

--- scriptlandia-examples-and-tools/projects/tech-examples/wicket/README.txt
+++ scriptlandia-examples-and-tools/projects/tech-examples/wicket/README.txt
@@ -1,2 +1,4 @@
 
 >mvn jetty:run
+
+firefox>http://localhost:9090/wicket-test/helloworld
--- scriptlandia-examples-and-tools/projects/tech-examples/wicket/src/main/java/wicket/examples/helloworld/HelloWorldApplication.java
+++ scriptlandia-examples-and-tools/projects/tech-examples/wicket/src/main/java/wicket/examples/helloworld/HelloWorldApplication.java
@@ -1,13 +1,44 @@
+/*
+ * $Id: HelloWorldApplication.java 460265 2006-04-16 13:36:52Z jdonnerstag $
+ * $Revision: 460265 $ $Date: 2006-04-16 15:36:52 +0200 (Sun, 16 Apr 2006) $
+ * 
+ * ==================================================================== Licensed
+ * under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the
+ * License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package wicket.examples.helloworld;
 
-import wicket.protocol.http.WebApplication;
+import wicket.examples.WicketExampleApplication;
 
-public class HelloWorldApplication extends WebApplication {
+/**
+ * WicketServlet class for hello world example.
+ * 
+ * @author Jonathan Locke
+ */
+public class HelloWorldApplication extends WicketExampleApplication
+{
+	/**
+	 * Constructor.
+	 */
+	public HelloWorldApplication()
+	{
 
-  public HelloWorldApplication() {}
-    
-  public Class getHomePage() {
-    return HelloWorld.class;
-  }
+	}
 
+	/**
+	 * @see wicket.Application#getHomePage()
+	 */
+	public Class getHomePage()
+	{
+		return HelloWorld.class;
+	}
 }
