98589ac195a58b572756de614156c0534af66a27
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2007-07-27 13:19:03 +0000

    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@445 9700c210-9314-0410-bd77-8714033700fb

--- scriptlandia/package.bat
+++ scriptlandia/package.bat
@@ -6,34 +6,39 @@ if exist "%USERPROFILE%\jlaunchpad\config.bat" @call "%USERPROFILE%\jlaunchpad\c
 
 echo ---### Java Specification Version: %JAVA_SPECIFICATION_VERSION%
 
-echo ---### Installing basic dependencies...
+rem echo ---### Installing basic dependencies...
 
-SET BASIC_CLASSPATH=%REPOSITORY_HOME%\org\sf\jlaunchpad\universal-launcher-common\%LAUNCHER_VERSION%\universal-launcher-common-%LAUNCHER_VERSION%.jar
-SET BASIC_CLASSPATH=%BASIC_CLASSPATH%;%REPOSITORY_HOME%\org\apache\maven\bootstrap\bootstrap-mini\2.0.7\bootstrap-mini-2.0.7.jar
-SET BASIC_CLASSPATH=%BASIC_CLASSPATH%;%REPOSITORY_HOME%\org\sf\jlaunchpad\pom-reader\%LAUNCHER_VERSION%\pom-reader-%LAUNCHER_VERSION%.jar
+rem SET BASIC_CLASSPATH=%REPOSITORY_HOME%\org\sf\jlaunchpad\universal-launcher-common\%LAUNCHER_VERSION%\universal-launcher-common-%LAUNCHER_VERSION%.jar
+rem SET BASIC_CLASSPATH=%BASIC_CLASSPATH%;%REPOSITORY_HOME%\org\apache\maven\bootstrap\bootstrap-mini\2.0.7\bootstrap-mini-2.0.7.jar
+rem SET BASIC_CLASSPATH=%BASIC_CLASSPATH%;%REPOSITORY_HOME%\org\sf\jlaunchpad\pom-reader\%LAUNCHER_VERSION%\pom-reader-%LAUNCHER_VERSION%.jar
 
+rem %JAVA_HOME%\bin\java ^
+rem   -Drepository.home=%REPOSITORY_HOME% ^
+rem   %PROXY_PARAMS% ^
+rem   -Dbasedir=projects\scriptlandia-startup ^
+rem   -classpath %BASIC_CLASSPATH% ^
+rem   %SYSTEM_PROPERTIES% ^
+rem   org.sf.pomreader.ProjectInstaller
 
-rem   -Dmaven.repo.local=%REPOSITORY_HOME% %PROXY_PARAMS% -Dbasedir=projects\scriptlandia-startup
+rem echo ---### Installing required projects and configuration files...
 
-%JAVA_HOME%\bin\java ^
-  -Drepository.home=%REPOSITORY_HOME% ^
-  %PROXY_PARAMS% ^
-  -Dbasedir=projects\scriptlandia-startup ^
-  -classpath %BASIC_CLASSPATH% ^
-  %SYSTEM_PROPERTIES% ^
-  org.sf.pomreader.ProjectInstaller
+rem SET CLASSPATH=%REPOSITORY_HOME%\org\apache\ant\ant-launcher\%ANT_VERSION%\ant-launcher-%ANT_VERSION%.jar
+rem SET CLASSPATH=%CLASSPATH%;%REPOSITORY_HOME%\org\apache\ant\ant\%ANT_VERSION%\ant-%ANT_VERSION%.jar
+rem SET CLASSPATH=%CLASSPATH%;%REPOSITORY_HOME%\org\apache\ant\ant-apache-bsf\%ANT_VERSION%\ant-apache-bsf-%ANT_VERSION%.jar
+rem SET CLASSPATH=%CLASSPATH%;%REPOSITORY_HOME%\org\apache\ant\ant-nodeps\%ANT_VERSION%\ant-nodeps-%ANT_VERSION%.jar
+rem SET CLASSPATH=%CLASSPATH%;%REPOSITORY_HOME%\commons-logging\commons-logging\1.0.4\commons-logging-1.0.4.jar
+rem SET CLASSPATH=%CLASSPATH%;%REPOSITORY_HOME%\bsf\bsf\2.4.0\bsf-2.4.0.jar
+rem SET CLASSPATH=%CLASSPATH%;%REPOSITORY_HOME%\bsh\bsh\%BEANSHELL_VERSION%\bsh-%BEANSHELL_VERSION%.jar
 
-echo ---### Installing required projects and configuration files...
+rem %JAVA_HOME%\bin\java ^
+rem   -classpath %CLASSPATH% ^
+rem   %SYSTEM_PROPERTIES% ^
+rem   org.apache.tools.ant.launch.Launcher -f package.ant package.projects
+rem 
 
-SET CLASSPATH=%REPOSITORY_HOME%\org\apache\ant\ant-launcher\%ANT_VERSION%\ant-launcher-%ANT_VERSION%.jar
-SET CLASSPATH=%CLASSPATH%;%REPOSITORY_HOME%\org\apache\ant\ant\%ANT_VERSION%\ant-%ANT_VERSION%.jar
-SET CLASSPATH=%CLASSPATH%;%REPOSITORY_HOME%\org\apache\ant\ant-apache-bsf\%ANT_VERSION%\ant-apache-bsf-%ANT_VERSION%.jar
-SET CLASSPATH=%CLASSPATH%;%REPOSITORY_HOME%\org\apache\ant\ant-nodeps\%ANT_VERSION%\ant-nodeps-%ANT_VERSION%.jar
-SET CLASSPATH=%CLASSPATH%;%REPOSITORY_HOME%\commons-logging\commons-logging\1.0.4\commons-logging-1.0.4.jar
-SET CLASSPATH=%CLASSPATH%;%REPOSITORY_HOME%\bsf\bsf\2.4.0\bsf-2.4.0.jar
-SET CLASSPATH=%CLASSPATH%;%REPOSITORY_HOME%\bsh\bsh\%BEANSHELL_VERSION%\bsh-%BEANSHELL_VERSION%.jar
+rem SET MAIN_CLASS=org.apache.tools.ant.launch.Launcher
+SET MAIN_CLASS=org.apache.tools.ant.Main
 
-%JAVA_HOME%\bin\java ^
-  -classpath %CLASSPATH% ^
-  %SYSTEM_PROPERTIES% ^
-  org.apache.tools.ant.launch.Launcher -f package.ant package.projects
+SET PROPERTIES="-deps.file.name=projects\scriptlandia-startup\pom.xml" "-main.class.name=%MAIN_CLASS%"
+
+%LAUNCHER_HOME%\launcher.bat %PROPERTIES% -f package.ant package.projects 
