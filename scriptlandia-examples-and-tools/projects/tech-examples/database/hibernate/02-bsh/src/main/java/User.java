ab308c5c961ce7807b86c7089acb6d2652303570
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2007-09-19 21:49:46 +0000

    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@553 9700c210-9314-0410-bd77-8714033700fb

---
+++ scriptlandia-examples-and-tools/projects/tech-examples/database/hibernate/02-bsh/src/main/java/User.java
@@ -0,0 +1,34 @@
+
+public class User {
+
+  private String name;
+  private int age;
+  private long id;
+
+  public User() {}
+
+  public String getName(){
+    return name;
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public int getAge(){
+    return age;
+  }
+
+  public void setAge(int age){
+    this.age = age;
+  }
+
+  public long getId(){
+    return id;
+  }
+
+  public void setId(long id){
+    this.id = id;
+  }
+
+}		
