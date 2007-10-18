624b9d4ffed9b236c8fb15a46c65a069be3c5d4e
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2007-10-18 14:16:08 +0000

    add clojure
    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@576 9700c210-9314-0410-bd77-8714033700fb

---
+++ scriptlandia-examples-and-tools/projects/lang-examples/clojure/test-clojure.bat
@@ -0,0 +1,9 @@
+SET JAVA_HOME=c:\Java\jdk1.5.0
+
+%JAVA_HOME%\bin\java -cp C:\Work\scriptlandia-repository\languages\clojure\clojure\20071016\clojure-20071016.jar clojure.lang.Compiler C:\Work\scriptlandia-repository\languages\clojure\boot\20071016\boot.clj
+
+pause
+
+
+
+
