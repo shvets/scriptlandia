7a12b1bbb08492d380c3ac08d1215fa4cebfe87f
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2007-12-08 22:39:13 +0000

    upgrading fortress version
    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@622 9700c210-9314-0410-bd77-8714033700fb

--- scriptlandia/projects/scriptlandia-config/bin/scriptlandia.bat
+++ scriptlandia/projects/scriptlandia-config/bin/scriptlandia.bat
@@ -1,18 +1,14 @@
 @echo off
 
-rem set JAVA_HOME=@java.home@
 set MOBILE_JAVA_HOME=@mobile.java.home@
 set SCRIPTLANDIA_HOME=@scriptlandia.home@
 set LAUNCHER_HOME=@launcher.home@
 
 SET NAILGUN=@repository.home@\com\martiansoftware\nailgun-bin\@nailgun.version@\ng
 
-rem set APP=%~nx0
-rem set APP_NAME=%APP:~0,-4%
-rem set APP=%SCRIPTLANDIA_HOME%\%APP:~0,-4%
-
 if "%APP_NAME%" == "" (
-  SET APP_NAME=scriptlandia
+  set APP=%~nx0
+  set APP_NAME=%APP:~0,-4%
 )
 
 SET MAIN_APP_CONF=%SCRIPTLANDIA_HOME%\%APP_NAME%.conf
