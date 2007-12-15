eea3632745648dd2bc1a697aa88a95e424b21a91
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2007-12-15 22:18:13 +0000

    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@637 9700c210-9314-0410-bd77-8714033700fb

--- scriptlandia/installer.bat
+++ scriptlandia/installer.bat
@@ -9,6 +9,39 @@ if not exist %LAUNCHER_HOME%\launcher.bat (
   exit
 )
 
+if not exist %REPOSITORY_HOME%\org\sf\jlaunchpad\jlaunchpad-launcher\%LAUNCHER_VERSION%\jlaunchpad-launcher-%LAUNCHER_VERSION%.jar (
+  echo Please run jlaunchpad-installer.bat first.
+
+  pause
+  exit
+)
+
+if not exist %REPOSITORY_HOME%\org\sf\jlaunchpad\jlaunchpad-common\%LAUNCHER_VERSION%\jlaunchpad-common-%LAUNCHER_VERSION%.jar (
+  echo Please run jlaunchpad-installer.bat first.
+
+  pause
+  exit
+)
+
+if not exist %REPOSITORY_HOME%\org\sf\jlaunchpad\pom-reader\%LAUNCHER_VERSION%\pom-reader-%LAUNCHER_VERSION%.jar (
+  echo Please run jlaunchpad-installer.bat first.
+
+  pause
+  exit
+)
+
+if not exist %REPOSITORY_HOME%\org\apache\maven\bootstrap\bootstrap-mini\2.0.8\bootstrap-mini-2.0.8.jar (
+  echo Please run jlaunchpad-installer.bat first.
+
+  pause
+  exit
+)
+
+SET SYSTEM_PROPERTIES1=%SYSTEM_PROPERTIES% "-deps.file.name=%REPOSITORY_HOME%/org/sf/jlaunchpad/jlaunchpad-launcher/%LAUNCHER_VERSION%/jlaunchpad-launcher-%LAUNCHER_VERSION%.pom" 
+SET SYSTEM_PROPERTIES1=%SYSTEM_PROPERTIES1% "-main.class.name=org.sf.pomreader.ProjectInstaller"
+
+@call %LAUNCHER_HOME%\launcher.bat %SYSTEM_PROPERTIES1% "-Dbasedir=projects/scriptlandia-installer" "-Dbuild.required=false"
+
 rem SET MAIN_CLASS=org.sf.scriptlandia.install.CoreInstaller
 SET MAIN_CLASS=org.sf.scriptlandia.install.GuiInstaller
 
