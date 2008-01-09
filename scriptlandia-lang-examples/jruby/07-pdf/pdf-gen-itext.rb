dc61bb5f064ee94877bfb3817673874889d6c6d5
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2008-01-09 20:39:07 +0000

    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@688 9700c210-9314-0410-bd77-8714033700fb

--- scriptlandia-lang-examples/jruby/07-pdf/pdf-gen-itext.rb
+++ scriptlandia-lang-examples/jruby/07-pdf/pdf-gen-itext.rb
@@ -2,7 +2,7 @@
 
 require 'java'
 
-require "d:/maven-repository/com/lowagie/itext/2.0.4/itext-2.0.4.jar"
+require "c:/maven-repository/com/lowagie/itext/2.0.4/itext-2.0.4.jar"
 
 include_class "java.io.FileOutputStream"
 include_class "java.io.IOException"
