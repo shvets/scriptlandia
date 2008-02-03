709b567613d0c43fd9049cf98435478f70903982
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2008-02-03 01:34:05 +0000

    renaming launcher to jlaunchpad
    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@723 9700c210-9314-0410-bd77-8714033700fb

--- scriptlandia/jlaunchpad-installer.bat
+++ scriptlandia/jlaunchpad-installer.bat
@@ -1,28 +1,19 @@
 @ECHO OFF
 
-@call config.bat
+SET CURRENT_DIR=%~dp0
 
-SET JLAUNCHPAD_PROJECT=.
-SET ORIGINAL_JLAUNCHPAD_PROJECT=..\..\..\jlaunchpad\trunk\launcher
+SET DRIVE_LETTER=c:
 
-if exist "%ORIGINAL_JLAUNCHPAD_PROJECT%\projects\jlaunchpad-launcher\target\jlaunchpad-launcher.jar" (
-  cd %ORIGINAL_JLAUNCHPAD_PROJECT%
+set JAVA_HOME=%DRIVE_LETTER%/Java/jdk1.6.0
+
+SET JLAUNCHPAD_VERSION=1.0.1
+
+SET JLAUNCHPAD_PROJECT=%CURRENT_DIR%jlaunchpad-%JLAUNCHPAD_VERSION%
+
+if not exist %JLAUNCHPAD_PROJECT% (
+  %JAVA_HOME%/bin/jar xvf jlaunchpad-%JLAUNCHPAD_VERSION%-install.zip
 )
 
-SET BOOTSTRAP_MINI_PROJECT=%JLAUNCHPAD_PROJECT%\projects\bootstrap-mini
-SET JLAUNCHPAD_COMMON_PROJECT=%JLAUNCHPAD_PROJECT%\projects\jlaunchpad-common
-SET POM_READER_PROJECT=%JLAUNCHPAD_PROJECT%\projects\pom-reader
-SET JLAUNCHPAD_LAUNCHER_PROJECT=%JLAUNCHPAD_PROJECT%\projects\jlaunchpad-launcher
-SET JDOM_PROJECT=%JLAUNCHPAD_PROJECT%\projects\jdom
+cd %JLAUNCHPAD_PROJECT%
 
-SET CLASSPATH=%BOOTSTRAP_MINI_PROJECT%\target\bootstrap-mini.jar
-SET CLASSPATH=%CLASSPATH%;%JLAUNCHPAD_COMMON_PROJECT%\target\jlaunchpad-common.jar
-SET CLASSPATH=%CLASSPATH%;%POM_READER_PROJECT%\target\pom-reader.jar
-SET CLASSPATH=%CLASSPATH%;%JLAUNCHPAD_LAUNCHER_PROJECT%\target\jlaunchpad-launcher.jar
-SET CLASSPATH=%CLASSPATH%;%JDOM_PROJECT%\target\jdom.jar
-
-
-REM SET MAIN_CLASS=org.sf.jlaunchpad.install.CoreInstaller
-SET MAIN_CLASS=org.sf.jlaunchpad.install.GuiInstaller
-
-%JAVA_HOME%\bin\java -classpath %CLASSPATH% %SYSTEM_PROPERTIES% -Dclassworlds.version=1.1 %MAIN_CLASS%
+%JLAUNCHPAD_PROJECT%\installer.bat
