f2506c9f1f3aaa4b7b66540599fb494d81788720
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2008-02-04 13:45:39 +0000

    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@736 9700c210-9314-0410-bd77-8714033700fb

---
+++ scriptlandia-lang-examples/apt/test3/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+  private String s1;
+
+  void m1() {}
+
+  class Test2 {
+    private String s2;
+
+    void m2() {}
+
+    class Test3 {
+      private String s3;
+
+      void m3() {}
+    }
+  }
+}
