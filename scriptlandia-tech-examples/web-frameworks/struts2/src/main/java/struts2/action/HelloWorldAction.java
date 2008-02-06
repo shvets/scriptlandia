bde269ae71dac6ffb59237ddca4e1faba6019f94
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2008-02-06 21:11:09 +0000

    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@744 9700c210-9314-0410-bd77-8714033700fb

---
+++ scriptlandia-tech-examples/web-frameworks/struts2/src/main/java/struts2/action/HelloWorldAction.java
@@ -0,0 +1,45 @@
+/*
+ * $Id: RequestUtils.java 394468 2006-04-16 12:16:03Z tmjee $
+ *
+ * Copyright 2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package struts2.action;
+
+import java.util.Date;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.validator.annotations.Validation;
+import com.opensymphony.xwork2.validator.annotations.RequiredStringValidator;
+import com.opensymphony.xwork2.validator.annotations.RequiredFieldValidator;
+import com.opensymphony.xwork2.conversion.annotations.Conversion;
+import com.opensymphony.xwork2.conversion.annotations.TypeConversion;
+
+public class HelloWorldAction extends ActionSupport {
+     public static final String MESSAGE = "Struts is up and running ...";
+
+    public String execute() throws Exception {
+        setMessage(MESSAGE);
+        return SUCCESS;
+    }
+
+    private String message;
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
