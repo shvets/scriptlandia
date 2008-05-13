665669b46a254d5474d0f070e42f48358ab9ce93
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2008-05-13 17:17:21 +0000

    new files
    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@797 9700c210-9314-0410-bd77-8714033700fb

---
+++ scriptlandia/config.bat
@@ -0,0 +1,7 @@
+rem config.bat
+
+rem Default values
+SET DRIVE_LETTER=c:
+
+SET JLAUNCHPAD_HOME=%DRIVE_LETTER%\Work\jlaunchpad
+SET REPOSITORY_HOME=%DRIVE_LETTER%\Work\maven-repository
---
+++ scriptlandia/config.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Default values
+
+JLAUNCHPAD_HOME=~/jlaunchpad
+REPOSITORY_HOME=/media/sda2/maven-repository
+
+export JLAUNCHPAD_HOME REPOSITORY_HOME
+
