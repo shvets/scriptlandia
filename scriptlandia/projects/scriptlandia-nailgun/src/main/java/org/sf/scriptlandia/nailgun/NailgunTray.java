e589fa09639a82cefe0379c6451343ab140e9bc0
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2008-05-14 19:50:23 +0000

    implementing NailgunTray in such a way that it can work with jdic and java6 deploy.jar
    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@803 9700c210-9314-0410-bd77-8714033700fb

--- scriptlandia/projects/scriptlandia-nailgun/src/main/java/org/sf/scriptlandia/nailgun/NailgunTray.java
+++ scriptlandia/projects/scriptlandia-nailgun/src/main/java/org/sf/scriptlandia/nailgun/NailgunTray.java
@@ -8,6 +8,8 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.net.InetAddress;
+import java.lang.reflect.Method;
+import java.lang.reflect.Constructor;
 
 /**
  * This class system tray for NG server.
@@ -15,118 +17,186 @@ import java.net.InetAddress;
  * @author Alexander Shvets
  * @version 1.0 12/16/2006
  */
-public class NailgunTray extends TrayIcon
-{
-  /** The popup frame that displays available menu choice. */
-  //private JFrame popupFrame;
+public class NailgunTray {
 
-  /** The popup menu. */
-  private java.awt.PopupMenu popupMenu;
-
-  /** The Nailgun server instance. */
+  /**
+   * The Nailgun server instance.
+   */
   private NGServer server;
+
+  private Object trayIcon;
+
+  private boolean isJava6OrMore = false;
 
   /**
    * Creates new system tray. /
+   *
+   * @throws Exception the exception
    */
-  public NailgunTray() {
-    //super(new ImageIcon(NailgunTray.class.getResource("/images/scriptlandia.gif")));
-    super(Toolkit.getDefaultToolkit().getImage(NailgunTray.class.getResource("/images/scriptlandia.gif")));
+  public NailgunTray() throws Exception {
+    String javaVersion = System.getProperty("java.version");
+    int versionNumber = Integer.parseInt(javaVersion.substring(2, 3));
 
-    this.setImageAutoSize(true);
+    if (versionNumber > 5) {
+      isJava6OrMore = true;
+    }
 
-    this.setToolTip("Scriptlandia Launching Pad");
-
-    createPopupMenu();
-
-    this.setPopupMenu(popupMenu);
-
-    /*this.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        showFloatingPane();
-      }
-    });
-*/
+    if (isJava6OrMore) {
+      trayIcon = createJava6TrayIcon();
+    } else {
+      trayIcon = createJdicTrayIcon();
+    }
   }
 
-  /**
-   * Creates popup menu.
-   */
-  private void createPopupMenu() {
-    popupMenu = new PopupMenu("Menu");
+  private Object createJava6TrayIcon() throws Exception {
+    Image image = Toolkit.getDefaultToolkit().getImage(NailgunTray.class.getResource("/images/scriptlandia.gif"));
 
-    MenuItem serverMenuItem = new MenuItem();
+    Class trayIconClass = Class.forName("java.awt.TrayIcon");
 
-    //setServerMenuItemTitle(serverMenuItem);
-    serverMenuItem.setLabel("Start Nailgun Server");
+    Constructor trayIconConstructor = trayIconClass.getConstructor(Image.class);
 
-    serverMenuItem.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        executeServerMenuItem((MenuItem) e.getSource());
-      }
-    });
+    Object trayIcon = trayIconConstructor.newInstance(image);
 
-    MenuItem exitMenuItem = new MenuItem("Exit");
+    Method setImageAutoSizeMethod = trayIconClass.getDeclaredMethod("setImageAutoSize", boolean.class);
+    setImageAutoSizeMethod.invoke(trayIcon, Boolean.TRUE);
+
+    Method setToolTipMethod = trayIconClass.getDeclaredMethod("setToolTip", String.class);
+    setToolTipMethod.invoke(trayIcon, "Scriptlandia Launching Pad");
+
+    PopupMenu popupMenu = new PopupMenu("Menu");
+
+    final MenuItem exitMenuItem = new MenuItem("Exit");
     exitMenuItem.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         exit();
       }
     });
 
+    MenuItem serverMenuItem = new MenuItem();
+
+    serverMenuItem.setLabel("Start Nailgun Server");
+
+    serverMenuItem.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        MenuItem menuItem = (MenuItem) e.getSource();
+
+        if (executeServerMenuItem()) {
+          menuItem.setLabel("Stop Nailgun Server");
+          exitMenuItem.setEnabled(false);
+        } else {
+          menuItem.setLabel("Start Nailgun Server");
+          exitMenuItem.setEnabled(true);
+        }
+      }
+    });
+
     popupMenu.add(serverMenuItem);
     popupMenu.add(exitMenuItem);
+
+    Method setPopupMenuMethod = trayIconClass.getDeclaredMethod("setPopupMenu", PopupMenu.class);
+    setPopupMenuMethod.invoke(trayIcon, popupMenu);
+
+    return trayIcon;
   }
 
-/*
-  private void showFloatingPane() {
-    if (popupFrame == null) {
-      popupFrame = new JFrame("Scriptlandia Launching Pad");
-      popupFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+  private Object createJdicTrayIcon() throws Exception {
+    Icon icon = new ImageIcon(NailgunTray.class.getResource("/images/scriptlandia.gif"));
 
-      JPanel panel = new JPanel();
+    Class trayIconClass = Class.forName("org.jdesktop.jdic.tray.TrayIcon");
+    Constructor trayIconConstructor = trayIconClass.getConstructor(Icon.class);
 
-      JLabel label = new JLabel("scriptlandia");
-      panel.add(label);
+    Object trayIcon = trayIconConstructor.newInstance(icon);
 
-      popupFrame.add(panel);
+    Method setCaptionMethod = trayIconClass.getDeclaredMethod("setCaption", String.class);
+    setCaptionMethod.invoke(trayIcon, "Scriptlandia Launching Pad");
 
-      popupFrame.pack();
+    JPopupMenu popupMenu = new JPopupMenu("Menu");
 
-      Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+    final JMenuItem exitMenuItem = new JMenuItem("Exit");
+    exitMenuItem.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        exit();
+      }
+    });
 
-      popupFrame.setLocation((int) (size.getWidth() - popupFrame.getWidth() - 30),
-              (int) (size.getHeight() - popupFrame.getHeight() - 30));
-    }
+    JMenuItem serverMenuItem = new JMenuItem();
 
-    if (!popupFrame.isVisible()) {
-      popupFrame.setVisible(true);
-    }
-    else {
-      popupFrame.setVisible(false);
+    serverMenuItem.setText("Start Nailgun Server");
+
+    serverMenuItem.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        JMenuItem menuItem = (JMenuItem) e.getSource();
+
+        if (executeServerMenuItem()) {
+          menuItem.setText("Stop Nailgun Server");
+          exitMenuItem.setEnabled(false);
+        } else {
+          menuItem.setText("Start Nailgun Server");
+          exitMenuItem.setEnabled(true);
+        }
+      }
+    });
+
+    popupMenu.add(serverMenuItem);
+    popupMenu.add(exitMenuItem);
+
+    Method setPopupMenuMethod = trayIconClass.getDeclaredMethod("setPopupMenu", JPopupMenu.class);
+    setPopupMenuMethod.invoke(trayIcon, popupMenu);
+
+    return trayIcon;
+  }
+
+  private void addToSystemTray() throws Exception {
+    if (isJava6OrMore) {
+      Class systemTrayClass = Class.forName("java.awt.SystemTray");
+      Class trayIconClass = Class.forName("java.awt.TrayIcon");
+
+      Method getSystemTrayMethod = systemTrayClass.getDeclaredMethod("getSystemTray");
+      Object systemTray = getSystemTrayMethod.invoke(trayIcon);
+
+      Method addMethod = systemTrayClass.getDeclaredMethod("add", trayIconClass);
+      addMethod.invoke(systemTray, trayIcon);
+    } else {
+      Class systemTrayClass = Class.forName("org.jdesktop.jdic.tray.SystemTray");
+      Class trayIconClass = Class.forName("org.jdesktop.jdic.tray.TrayIcon");
+
+      Method getDefaultSystemTrayMethod = systemTrayClass.getDeclaredMethod("getDefaultSystemTray");
+      Object systemTray = getDefaultSystemTrayMethod.invoke(trayIcon);
+
+      Method addTrayIconMethod = systemTrayClass.getDeclaredMethod("addTrayIcon", trayIconClass);
+      addTrayIconMethod.invoke(systemTray, trayIcon);
     }
   }
-*/
 
-  /**
-   * Sets the menu label based on inner state.
-   *
-   * @param menuItem the menu item
-   */
-/*  private void setServerMenuItemTitle(JMenuItem menuItem) {
-    if (server != null && server.isRunning()) {
-      menuItem.setText("Stop Nailgun Server");
-    }
-    else {
-      menuItem.setText("Start Nailgun Server");
+  private void displayMessage(String message) {
+    try {
+      if (isJava6OrMore) {
+        Class trayIconClass = Class.forName("java.awt.TrayIcon");
+        Class trayIconMessageTypeInfoClass = Class.forName("java.awt.TrayIcon$MessageType");
+
+        Method valueOfMethod = trayIconMessageTypeInfoClass.getDeclaredMethod("valueOf", String.class);
+        Object infoMessageType = valueOfMethod.invoke(trayIcon, "INFO");
+
+        Method displayMessageMethod = trayIconClass.getDeclaredMethod("displayMessage", String.class, String.class, trayIconMessageTypeInfoClass);
+        displayMessageMethod.invoke(trayIcon, message, "Nailgun Server", infoMessageType);
+      } else {
+        Class trayIconClass = Class.forName("org.jdesktop.jdic.tray.TrayIcon");
+
+        Method displayMessageMethod = trayIconClass.getDeclaredMethod("displayMessage", String.class, String.class, int.class);
+        displayMessageMethod.invoke(trayIcon, message, "Nailgun Server", 0);
+      }
+
+    } catch (Exception e) {
+      e.printStackTrace();
     }
   }
-*/
+
   /**
    * Starting/stopping NG server operation.
    *
-   * @param menuItem the menu item
+   * @return true if server started; false if server stopped
    */
-  private void executeServerMenuItem(MenuItem menuItem) {
+  private boolean executeServerMenuItem() {
     if (server == null || !server.isRunning()) {
       InetAddress serverAddress = null;
       int port = NGConstants.DEFAULT_PORT;
@@ -163,19 +233,15 @@ public class NailgunTray extends TrayIcon
       }
 
       displayMessage("Nailgun Server started on "
-                + ((serverAddress == null)
-                  ? "all interfaces" : serverAddress.getHostAddress()) + ", port " + server.getPort() + ".",
-                   "Nailgun Server", MessageType.INFO);
-      menuItem.setLabel("Stop Nailgun Server");
-    }
-    else {
+          + ((serverAddress == null)
+          ? "all interfaces" : serverAddress.getHostAddress()) + ", port " + server.getPort() + ".");
+      return true;
+    } else {
       server.shutdown(false);
-      displayMessage("Nailgun Server stopped.", "Nailgun Server", MessageType.INFO);
+      displayMessage("Nailgun Server stopped.");
 
-      menuItem.setLabel("Start Nailgun Server");
+      return false;
     }
-
-    //setServerMenuItemTitle(menuItem);
   }
 
   /**
@@ -194,21 +260,21 @@ public class NailgunTray extends TrayIcon
     // Catch exceptions and exit, which prevents an orphaned tray icon
     // that might force the user to use Task Mgr to kill the process
     Thread.setDefaultUncaughtExceptionHandler(
-            new Thread.UncaughtExceptionHandler() {
-              public void uncaughtException(Thread t, Throwable e) {
-                e.printStackTrace();
-                System.exit(1);
-              }
-            });
+        new Thread.UncaughtExceptionHandler() {
+          public void uncaughtException(Thread t, Throwable e) {
+            e.printStackTrace();
+            System.exit(1);
+          }
+        });
 
     SwingUtilities.invokeLater(new Runnable() {
       public void run() {
-        SystemTray systemTray = SystemTray.getSystemTray();
-
         try {
-          systemTray.add(new NailgunTray());
+          NailgunTray nailgunTray = new NailgunTray();
+
+          nailgunTray.addToSystemTray();
         }
-        catch (AWTException e) {
+        catch (Exception e) {
           e.printStackTrace();
         }
       }
