d554582488693c4fafe14915b4d8ee0cd5b84420
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2008-05-30 21:06:31 +0000

    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@811 9700c210-9314-0410-bd77-8714033700fb

--- scriptlandia/config.bat
+++ scriptlandia/config.bat
@@ -3,5 +3,5 @@ rem config.bat
 rem Default values
 SET DRIVE_LETTER=c:
 
-SET JLAUNCHPAD_HOME=%DRIVE_LETTER%\jlaunchpad
-SET REPOSITORY_HOME=%DRIVE_LETTER%\maven-repository
+SET JLAUNCHPAD_HOME=%DRIVE_LETTER%\Work\jlaunchpad
+SET REPOSITORY_HOME=%DRIVE_LETTER%\Work\maven-repository
