123312da3a52b6d385ce18e3027abc95764dc516
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2008-07-29 11:26:43 +0000

    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@909 9700c210-9314-0410-bd77-8714033700fb

--- scriptlandia-tech-examples/ruby/03-grooming-salon/GroomingSalon/app/controllers/application.rb
+++ scriptlandia-tech-examples/ruby/03-grooming-salon/GroomingSalon/app/controllers/application.rb
@@ -2,11 +2,13 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  #model :user
+
   helper :all # include all helpers, all the time
 
   layout "grooming-salon-layout"
 
-  before_filter :authenticate
+  before_filter :storeUserInSession
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
@@ -17,13 +19,26 @@ class ApplicationController < ActionController::Base
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
 
-
   protected 
-  
-  def authenticate
-    authenticate_or_request_with_http_basic do |user_name, password|
-      user_name == "foo" && password == "bar"
-    end
+
+  def storeUserInSession
+    #user_name  = "foo";
+    #password = "bar"
+
+    ok = authenticate()
+
+    session[:user] = @current_user
   end
 
+  def authenticate()
+    authenticate_or_request_with_http_basic do |user_name, password|
+      #ok = (un == user_name && p == password)
+       puts "1" + user_name
+      @current_user = User.find(:first, :conditions => ["name =?", user_name])
+      #puts "2" + @current_user.to_s
+      #@current_user != null 
+      # && @current_user.password == password
+      true
+    end
+   end
 end
