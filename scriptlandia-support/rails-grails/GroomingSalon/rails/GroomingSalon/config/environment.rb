6cdab2a37f2cb2f02eb0df07f97a494bb372af36
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2008-10-02 03:10:13 +0000

    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@1015 9700c210-9314-0410-bd77-8714033700fb

--- scriptlandia-support/rails-grails/GroomingSalon/rails/GroomingSalon/config/environment.rb
+++ scriptlandia-support/rails-grails/GroomingSalon/rails/GroomingSalon/config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.1.1' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -106,8 +106,8 @@ GeoKit::Geocoders::timeout = 3
 # These setting can be nil if not needed, otherwise, addr and port must be 
 # filled in at a minimum.  If the proxy requires authentication, the username
 # and password can be provided as well.
-GeoKit::Geocoders::proxy_addr = "10.226.67.31"
-GeoKit::Geocoders::proxy_port = 8000
+#GeoKit::Geocoders::proxy_addr = 
+#GeoKit::Geocoders::proxy_port = 
 GeoKit::Geocoders::proxy_user = nil
 GeoKit::Geocoders::proxy_pass = nil
 
