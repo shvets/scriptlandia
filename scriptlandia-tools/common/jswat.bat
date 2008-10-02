f65681b84fdb0f0871d9899961f2fd02cbd512f0
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2008-10-02 14:50:26 +0000

    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@1018 9700c210-9314-0410-bd77-8714033700fb

---
+++ scriptlandia-tools/common/jswat.bat
@@ -0,0 +1,3 @@
+SET JSWAT_HOME=C:\Work\Programs\jswat-4.3
+
+%JSWAT_HOME%\bin\jswat %*
---
+++ scriptlandia-tools/common/shoes.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+
+SET SHOES_HOME=C:\Program Files\Common Files\Shoes\0.r395
+
+"%SHOES_HOME%\shoes.exe" %*
