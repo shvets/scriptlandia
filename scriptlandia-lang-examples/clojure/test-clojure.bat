f3ea283843776e25d6d8d014ab77863fa8d3b077
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2008-10-02 14:13:17 +0000

    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@1016 9700c210-9314-0410-bd77-8714033700fb

--- scriptlandia-lang-examples/clojure/test-clojure.bat
+++ scriptlandia-lang-examples/clojure/test-clojure.bat
@@ -1,4 +1,4 @@
 SET JAVA_HOME=c:\Java\jdk1.5.0
 
 
-%JAVA_HOME%\bin\java -cp C:\maven-repository\clojure\clojure\20071203\clojure-20071203.jar clojure.lang.Script C:\maven-repository\clojure\boot\20071203\boot.clj test-date.clj
+%JAVA_HOME%\bin\java -cp C:\Work\maven-repository\clojure\clojure\20080916\clojure-20080916.jar clojure.lang.Script test-date.clj
