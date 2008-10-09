dd1539a3c0a2ebe16b7e1416f0465456cacf6210
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2008-10-09 15:43:56 +0000

    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@1042 9700c210-9314-0410-bd77-8714033700fb

---
+++ scriptlandia-tech-examples/DP/creational/abstract-factory.rb
@@ -0,0 +1,80 @@
+# abstract-factory.rb
+
+# Provides an interface for creationg families of related or dependent objects without specifying
+# their concrete class.
+#
+# Provides one level of interface higher than the factory pattern. It is used to return one
+# of several factories.
+
+# 1. common interface for type and it's specializations
+
+class Alive 
+  def live
+  end
+end
+
+class Plant < Alive
+end
+
+class Animal < Alive
+end
+
+# 2. implementations of specializations
+
+class Roze < Plant 
+  def live
+    puts "roze" 
+  end
+end
+
+class Elephant < Animal 
+  def live
+    puts "elephant"
+  end
+end
+
+# 3. interface for factory (abstract factory)
+
+class AliveFactory 
+  def create_alive
+  end
+end
+
+# 4. different factories implementations
+
+class PlantFactory < AliveFactory 
+  def create_alive
+    Roze.new
+  end
+end
+
+class AnimalFactory < AliveFactory 
+  def create_alive
+    Elephant.new
+  end
+end
+
+# 5. factories manager (optional)
+
+class AliveManager 
+  def create_alive_factory(type)
+    if(type == 1) 
+      PlantFactory.new
+    elsif(type == 2) 
+      AnimalFactory.new
+    end
+  end 
+end
+
+# 6. test
+
+alive_manager = AliveManager.new
+
+alive_factory1 = alive_manager.create_alive_factory(1)
+alive_factory2 = alive_manager.create_alive_factory(2)
+
+alive1 = alive_factory1.create_alive
+alive2 = alive_factory2.create_alive
+
+alive1.live
+alive2.live
