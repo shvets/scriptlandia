1e2e1fbb941e4646ba9867db889258d25f3cf5c3
shvets <shvets@9700c210-9314-0410-bd77-8714033700fb>
2008-11-03 18:49:59 +0000

    git-svn-id: svn://svn.code.sf.net/p/scriptlandia/code/trunk@1070 9700c210-9314-0410-bd77-8714033700fb

--- scriptlandia-tech-examples/ruby/09-scriptlandia-r/install.rb
+++ scriptlandia-tech-examples/ruby/09-scriptlandia-r/install.rb
@@ -1,10 +1,30 @@
+#
+
 require 'rbconfig'
 require 'find'
 require 'ftools'
 
 include Config
 
-$ruby = CONFIG['ruby_install_name']
+def prepare
+  $bindir = CONFIG["bindir"]
+  $ruby = CONFIG['ruby_install_name']
+
+  $realbindir = $bindir
+
+  $sitedir = CONFIG["sitelibdir"]
+  unless $sitedir
+    version = CONFIG["MAJOR"]+"."+CONFIG["MINOR"]
+    $libdir = File.join(CONFIG["libdir"], "ruby", version)
+    $sitedir = $:.find {|x| x =~ /site_ruby/}
+    if !$sitedir
+      $sitedir = File.join($libdir, "site_ruby")
+    elsif $sitedir !~ Regexp.quote(version)
+      $sitedir = File.join($sitedir, version)
+    end
+  end
+
+end
 
 ##
 # Install a binary file. We patch in on the way through to
@@ -14,8 +34,7 @@ $ruby = CONFIG['ruby_install_name']
 # and let file associations to their stuff
 #
 
-def installBIN(from, opfile)
-
+def tmp_file
   tmp_dir = nil
   for t in [".", "/tmp", "c:/temp", $bindir]
     stat = File.stat(t) rescue next
@@ -26,63 +45,31 @@ def installBIN(from, opfile)
   end
 
   fail "Cannot find a temporary directory" unless tmp_dir
-  tmp_file = File.join(tmp_dir, "_tmp")
-    
-  File.open(from) do |ip|
-    File.open(tmp_file, "w") do |op|
-      ruby = File.join($realbindir, $ruby)
-      op.puts "#!#{ruby} -w"
-      op.write ip.read
+
+  File.join(tmp_dir, "_tmp")
+end
+
+def installBin(from, to, insert_line)
+  tmp = tmp_file()
+
+  if(insert_line)
+    File.open(from) do |ip|
+      File.open(tmp, "w") do |op|
+        ruby = File.join($realbindir, $ruby)
+        op.puts "#!#{ruby} -w"
+        op.write ip.read
+      end
     end
-  end
 
-  opfile += ".rb" if CONFIG["target_os"] =~ /mswin/i
-  File::install(tmp_file, File.join($bindir, opfile), 0755, true)
-  File::unlink(tmp_file)
-end
-
-$sitedir = CONFIG["sitelibdir"]
-unless $sitedir
-  version = CONFIG["MAJOR"]+"."+CONFIG["MINOR"]
-  $libdir = File.join(CONFIG["libdir"], "ruby", version)
-  $sitedir = $:.find {|x| x =~ /site_ruby/}
-  if !$sitedir
-    $sitedir = File.join($libdir, "site_ruby")
-  elsif $sitedir !~ Regexp.quote(version)
-    $sitedir = File.join($sitedir, version)
+    #opfile += ".rb" if CONFIG["target_os"] =~ /mswin/i
+    File::install(tmp, File.join($bindir, to), 0755, true)
+    File::unlink(tmp)
+  else
+    File::install(from, File.join($bindir, to), 0755, true)
   end
 end
 
-$bindir =  CONFIG["bindir"]
+prepare
 
-$realbindir = $bindir
-
-bindir = CONFIG["bindir"]
-if (destdir = ENV['DESTDIR'])
-  $bindir  = destdir + $bindir
-  $sitedir = destdir + $sitedir
-  
-  File::makedirs($bindir)
-  File::makedirs($sitedir)
-end
-
-rake_dest = File.join($sitedir, "rake")
-File::makedirs(rake_dest, true)
-File::chmod(0755, rake_dest)
-
-# The library files
-
-files = Dir.chdir('lib') { Dir['**/*.rb'] }
-
-for fn in files
-  fn_dir = File.dirname(fn)
-  target_dir = File.join($sitedir, fn_dir)
-  if ! File.exist?(target_dir)
-    File.makedirs(target_dir)
-  end
-  File::install(File.join('lib', fn), File.join($sitedir, fn), 0644, true)
-end
-
-# and the executable
-
-installBIN("bin/rake", "rake")
+installBin("bin/sl.bat", "sl.bat", false)
+installBin("bin/sl", "sl", true)
